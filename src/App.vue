d92444da99ecfcf51b83b440c4867e18fb24e589
Fake Name <fakename@fake.com>
2024-04-01 08:37:16 +1100

    Second commit, add Epilogue font, add image fold

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,97 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue"; // Import ref from Vue
+
+
+import Post_6 from "./components/Post_6.vue"
+import Post_5 from "./components/Post_5.vue";
+import Post_4 from "./components/Post_4.vue";
+import Post_3 from "./components/Post_3.vue";
+import Post_2 from "./components/Post_2.vue";
+import Post_1 from "./components/Post_1.vue";
+
+const numPosts = 6; // Number of posts
+const showPostIndex = ref(numPosts); // Variable to track which post is shown, initialized to the last post index
+
+const toggleShow = (index: number) => {
+  showPostIndex.value = index;
+};
+
+// Array of post titles
+const postTitles = ref<string[]>([]);
+// Populate post titles with template titles
+// for (let i = 0; i < numPosts; i++) {
+//   postTitles.value.push(`Post ${numPosts - i} title`);
+// }
+postTitles.value[numPosts - 1] = "Post 1 title";
+postTitles.value[numPosts - 2] = "Post 2 title";
+postTitles.value[numPosts - 3] = "Post 3 title";
+postTitles.value[numPosts - 4] = "Post 4 title";
+postTitles.value[numPosts - 5] = "Post 5 title";
+postTitles.value[numPosts - 6] = "Post 6 title";
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="header">
+    <h1>Header text goes here</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div class="sidebar">
+    <h3>Recent Posts</h3>
+    <br />
+    
+    <p v-for="(title, index) in postTitles" :key="index">
+      <button
+        v-if="showPostIndex !== numPosts - index"
+        @click="toggleShow(numPosts - index)"
+      >
+        {{ title }}
+      </button>
+    </p>
+  </div>
+
+  <div class="main">
+    <!-- Display the component based on the current post index -->
+    <template v-if="showPostIndex === 6">
+      <Post_6 />
+    </template>
+    <template v-else-if="showPostIndex === 5">
+      <Post_5 />
+    </template>
+    <template v-else-if="showPostIndex === 4">
+      <Post_4 />
+    </template>
+    <template v-else-if="showPostIndex === 3">
+      <Post_3 />
+    </template>
+    <template v-else-if="showPostIndex === 2">
+      <Post_2 />
+    </template>
+    <template v-else-if="showPostIndex === 1">
+      <Post_1 />
+    </template>
+    <!-- Add more templates for other posts if needed -->
+  </div>
+
+  <footer><h1>.....</h1></footer>
 </template>
 
 <style scoped>
+button {
+  min-width: 100%;
+}
+
+button:hover {
+  filter: drop-shadow(0 0 2em #fdfdfdaa);
+  background-color: bisque;
+  min-width: 100%;
+}
+
+button.inactive {
+  background-color: rgb(0, 0, 0);
+  height: 0px;
+  width: 0px;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -24,6 +101,7 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ src/components/Post_6.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <article>
+ <h2>Example most recent post</h2>
+
+<figure class="mainImage">
+ <img src="/Post_6_images/Example_main_image.png" alt="">
+ <figcaption>Post 6 main image caption goes in here</figcaption>
+</figure>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, eveniet assumenda est amet accusamus laboriosam reiciendis magnam deleniti dignissimos at nam placeat autem officiis alias. Sed tempora magnam perspiciatis repellendus animi laboriosam possimus. Earum quis, nemo, rerum odit animi repudiandae distinctio nesciunt voluptatibus fugiat libero cupiditate quos nostrum laudantium illo tenetur. Ducimus, assumenda quaerat ipsum possimus recusandae accusantium quibusdam quasi veritatis inventore ullam voluptate excepturi velit natus eos nesciunt molestiae itaque est commodi nobis deserunt tempore. Porro quo veniam voluptatibus facere? Omnis ex laudantium, dolore et debitis, maxime ut sint quam delectus nemo, eaque officia quo sapiente voluptatum facilis enim odit. Tenetur similique aperiam culpa cumque id ad excepturi deleniti! Error praesentium ut nam. Labore et ipsum ullam quisquam. Doloremque beatae provident nisi eos! Atque odio quis, maiores, beatae, voluptatem quia ea explicabo corporis perferendis est aliquid fugit quod voluptate doloribus ducimus molestiae vero. Nostrum, veritatis magni dolor accusamus rerum voluptas itaque, harum in pariatur voluptate autem ad recusandae! Sint dolores tenetur quisquam a nam ut, dolorem excepturi praesentium illum sunt, repudiandae inventore perspiciatis expedita explicabo distinctio consequuntur ea magni quia unde et cum ab ipsum vero? Deleniti minus dolor sit nisi molestiae? Vitae, accusantium molestiae ipsam dignissimos architecto tempora. Voluptatum harum totam fuga alias! Sit, quis ab dolorem pariatur maxime illo, rem obcaecati consequatur iure dolorum fugiat? Dolorum corporis quidem, ratione veniam perspiciatis libero non? Architecto modi eligendi quo laborum autem repellendus consequatur culpa adipisci cum. Beatae porro veritatis veniam ipsam. Similique quidem rem eveniet beatae dolore quibusdam inventore sint! Totam fuga ducimus odio illum labore eos, ut quas iste at tempora, doloribus sunt distinctio, consectetur illo dolore blanditiis. Minima aliquid harum ipsum enim ipsam earum ut beatae explicabo non dolore dicta deserunt expedita nam adipisci, eos ratione voluptatum illo. Deleniti molestias odio, modi illo tenetur facilis nostrum quo?
+<figure class="rightImage">
+ <img src="/Post_6_images/Example_right_image.png" alt="">
+ <figcaption>Post 6 right image caption goes in here</figcaption>
+</figure>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quidem earum iusto possimus blanditiis repellat laborum ratione repellendus magnam nisi, suscipit maxime temporibus dignissimos voluptate nulla architecto in consectetur a qui dicta rerum. Commodi dolorem nobis libero nemo assumenda facere eos sit officia, inventore error rem distinctio eaque quia labore reprehenderit laborum doloribus, doloremque soluta provident ex atque! Totam, ab illum maiores quisquam magni temporibus expedita accusamus nulla, sunt a laboriosam alias cum deleniti? Tenetur, obcaecati voluptatum? Velit quis, excepturi dolor corrupti fugit earum et assumenda quisquam ad expedita a aliquam tempore enim nam eveniet sunt illo maiores officiis atque in ex eligendi. Laboriosam doloremque voluptas rerum quia laborum aliquam. Vel vitae pariatur obcaecati quo suscipit, sint excepturi nemo voluptatum eligendi ex nam ipsum exercitationem praesentium? Quod quidem repellendus, doloremque neque eveniet similique accusantium magni ea iure expedita possimus ab dolorem ut quaerat dolorum, autem saepe. Voluptatem magnam quo ratione numquam eveniet reprehenderit labore nobis. Consectetur ducimus tempora distinctio eius culpa sed eveniet pariatur saepe veniam aperiam recusandae minus error similique aliquam provident et numquam, quisquam perspiciatis atque. Inventore tenetur amet adipisci vero sunt eum est error unde voluptas cum explicabo sit repellat assumenda iste consequuntur nisi eveniet quidem a ut, perspiciatis molestiae eos distinctio eaque! Aliquam eaque sit dolore illum molestias. Sequi esse animi eos adipisci iusto officiis quas vel ab alias nam, aliquid totam explicabo perferendis nihil natus fugit labore commodi deleniti blanditiis, quis optio porro quisquam eum quasi? Consectetur dicta vel maiores, facilis odio vitae minus, illum illo commodi aliquam, error vero quasi rerum tenetur placeat? Ut, aperiam ipsum hic placeat sed eveniet alias maxime qui iusto corporis nam, necessitatibus deleniti perspiciatis cum eaque ipsa recusandae reprehenderit. Explicabo distinctio consequatur ullam quod maxime excepturi minus reiciendis rerum porro sunt eveniet, saepe ab accusamus hic velit nemo iusto impedit delectus. Voluptatem praesentium ducimus ad libero voluptatum. Blanditiis mollitia atque accusantium omnis temporibus distinctio eos laboriosam expedita, perferendis ex recusandae nemo quia architecto maiores a, quasi quaerat. Praesentium eveniet tempora qui fugiat harum. Corrupti, dignissimos! Nihil deleniti repellat doloribus libero autem suscipit, beatae deserunt expedita. Quam eveniet in rem voluptatem aut, cumque quaerat eligendi, deleniti, molestias a doloribus magni nam recusandae exercitationem ducimus aspernatur ut neque! Ipsam non velit aliquid illum dolores illo vero consequatur. Blanditiis cum incidunt error laborum autem illum quos consequuntur architecto perferendis est odio perspiciatis amet, ipsa minus omnis provident nostrum, maxime, culpa molestias pariatur fugiat! Ipsum corrupti tempore sunt? Ex corporis esse saepe. Quis, amet. Repellendus autem quos animi dicta molestias perferendis doloribus eius ipsum, similique ex, inventore exercitationem quia architecto ducimus ullam perspiciatis fugit labore minima non, iste saepe libero minus cupiditate? Ut facilis nihil facere doloribus delectus odio possimus illo accusamus voluptatum, maxime recusandae magni soluta quis exercitationem reiciendis eius. A ducimus reprehenderit sed voluptatum asperiores quibusdam quis, molestias nisi blanditiis provident esse corrupti rerum? Molestiae aspernatur minima labore debitis, nostrum dolore. Illum quo fugit similique dolorem veritatis temporibus nihil, consequuntur iste esse aut sint ullam dolorum odit, culpa mollitia quisquam placeat?
+<figure class="leftImage">
+ <img src="/Post_6_images/Example_left_image.png" alt="">
+ <figcaption>Post 6 left image caption goes in here</figcaption>
+</figure>
+
+
+
+
+
+
+
+
+
+           <div style="clear:both;"></div> <!-- Clear floats -->
+      </article>
+      
+      </div>
+      
+      
+
+
+
+</template>
+
+<style scoped>
+
+
+</style>
